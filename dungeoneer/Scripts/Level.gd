5ca464588bff0f9c1af29a04bca57d3b3155e034
Taveon <TaveonFrog09@gmail.com>
2025-07-30 14:14:22 -0400

    Procedural connected rooms around player

--- dungeoneer/Scripts/Level.gd
+++ dungeoneer/Scripts/Level.gd
@@ -7,12 +7,10 @@ var turn : int = 0
 @onready var Hints = $Hints
 @onready var Info = $Info
 @onready var Map : TileMapLayer = $TileMapLayer
-var MAP_HEIGHT : int = 30
-var MAP_WIDTH : int = 60
-var ROOM_MAX : int = 12
-var ROOM_MIN : int = 6
-var MAX_ROOMS : int = 7
-var MIN_ROOMS : int = 4
+var MAP_HEIGHT : int = 40
+var MAP_WIDTH : int = 75
+var ROOM_RADII : Array[int] = [1, 10]
+var STEPS : int = 4
 var is_moving = false
 var is_attack = false
 
@@ -20,18 +18,30 @@ func _ready() -> void:
 	generate_level()
 
 func generate_level():
-	for x in range(MAP_WIDTH):
-		for y in range(MAP_HEIGHT):
+	for x in MAP_WIDTH + 1:
+		for y in MAP_HEIGHT + 1:
 			Map.set_cell(Vector2i(x, y), 0, Vector2i(4, 3))
-	var num_rooms = randi_range(MIN_ROOMS, MAX_ROOMS)
-	for room in num_rooms:
-		var room_size : Vector2i = Vector2i(randi_range(ROOM_MIN, ROOM_MAX), randi_range(ROOM_MIN, ROOM_MAX))
-		var room_origin = Vector2i(randi_range(0, MAP_WIDTH), randi_range(0, MAP_HEIGHT))
-		for x in range(room_origin.x, room_origin.x + room_size.x):
-			for y in range(room_origin.y, room_origin.y + room_size.y):
-				if x > 0 and y > 0 and x < MAP_WIDTH - 1 and y < MAP_HEIGHT - 1:
-					Map.erase_cell(Vector2i(x, y))
-		
+	
+	var room_points : Array[Vector2i] = [Map.local_to_map(Player.global_position)]
+	for step in STEPS:
+		var new_points : Array[Vector2i] = []
+		while room_points.size() > 0:
+			var point = room_points.pop_front()
+			var top_radius = ROOM_RADII.pick_random()
+			var left_radius = ROOM_RADII.back() - top_radius + ROOM_RADII.front()
+			var top : int = point.y - top_radius
+			var bottom : int = point.y + top_radius
+			var left : int = point.x - left_radius
+			var right : int = point.x + left_radius
+			for x in [left, right]:
+				for y in [top, bottom]:
+					if (x > 0 and y > 0 and x < MAP_WIDTH and y < MAP_HEIGHT) or Map.get_cell_source_id(Vector2i(x, y)) != -1:
+						new_points.append(Vector2i(x, y))
+			for x in range(left, right + 1):
+				for y in range(top, bottom + 1):
+					if x > 0 and y > 0 and x < MAP_WIDTH and y < MAP_HEIGHT:
+						Map.erase_cell(Vector2i(x, y))
+		room_points.append_array(new_points)
 
 func _on_submit_pressed() -> void:
 	for enemy in Enemies.get_children():
--- dungeoneer/Scripts/origin.gd
+++ dungeoneer/Scripts/origin.gd
@@ -5,7 +5,6 @@ func _ready() -> void:
 		for y in range(-1, 2):
 			var offset = Vector2(x, y)
 			if offset != Vector2.ZERO:
-				print("rect")
 				var rect : ColorRect = ColorRect.new()
 				rect.size = Vector2(16, 16)
 				rect.color = Color.WHITE
